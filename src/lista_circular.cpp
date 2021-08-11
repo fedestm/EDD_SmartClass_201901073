2a452b621175be11299068059450621a3f0af311
fedegy <fede88662@gmail.com>
2021-08-11 17:55:11 -0600

    Se valida que el nodo ya contiene datos

--- src/lista_circular.cpp
+++ src/lista_circular.cpp
@@ -19,11 +19,29 @@ void lista_circular::insertar(string carnet,string dpi,string nombre,string carr
     nuevo->siguiente=NULL;
     nuevo->anterior=NULL;
 
+    //Se valida sin el primer nodo esta vacio
     if(this->primero==NULL){
         nuevo->siguiente=nuevo;
         nuevo->anterior=nuevo;
         primero=nuevo;
         cout<<"Se inserto estudiante"<<endl;
+    }else{
+        //Si ya se encuentra un dato se guarda en el ultimo nodo
+        ultimo=primero->anterior;
+        //Se agregan los datos a dicho nodo
+        nuevo->carnet=carnet;
+        nuevo->dpi=dpi;
+        nuevo->nombre=nombre;
+        nuevo->carrera=carrera;
+        nuevo->pass=pass;
+        nuevo->creditos=creditos;
+        nuevo->edad=edad;
+        nuevo->correo=correo;
+        nuevo->siguiente=primero;
+        primero->anterior=nuevo;    //El puntero anterior se va al primero
+        nuevo->anterior=ultimo;     //El puntero anterior se convierte en un nodo nuevo
+        ultimo->siguiente=nuevo;    //El puntero siguiente se convierte en un nodo nuevo despues del puntero anterior
+
     }
 }
 
