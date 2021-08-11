c729caa8bca7f629230b20e44916f6ad823774c2
fedegy <fede88662@gmail.com>
2021-08-11 16:25:52 -0600

    Se agrego apuntadores primero y ultimo de lista circular

--- include/lista_circular.h
+++ include/lista_circular.h
@@ -1,12 +1,16 @@
 #ifndef LISTA_CIRCULAR_H
 #define LISTA_CIRCULAR_H
-
+#include "nodo_circular.h"
 
 class lista_circular
 {
     public:
         lista_circular();
         virtual ~lista_circular();
+
+        //Apuntadores Primero y Ultimo
+        nodo_circular *primero;
+        nodo_circular *ultimo;
 
     protected:
 
