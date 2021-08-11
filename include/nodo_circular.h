4a94f6d3e251bd1efa2c3b790331f43528dc709f
fedegy <fede88662@gmail.com>
2021-08-11 13:31:25 -0600

    Se agrego variables de estudiantes en (nodo_circular.h)

--- include/nodo_circular.h
+++ include/nodo_circular.h
@@ -1,12 +1,15 @@
 #ifndef NODO_CIRCULAR_H
 #define NODO_CIRCULAR_H
-
+#include <iostream>
+using namespace std;
 
 class nodo_circular
 {
     public:
         nodo_circular();
         virtual ~nodo_circular();
+        string carnet,dpi,nombre,carrera,pass;
+        int creditos,edad;
 
     protected:
 
