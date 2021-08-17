495dc32bafdcc976467ec2bb733c6ff1256ae0b7
fedegy <fede88662@gmail.com>
2021-08-16 19:18:45 -0600

    Se agregaron apuntadores y contador  (cola.h)

---
+++ include/cola.h
@@ -0,0 +1,19 @@
+#ifndef COLA_H
+#define COLA_H
+
+
+class cola
+{
+    public:
+        cola();
+        virtual ~cola();
+        nodo_cola *primero;
+        nodo_cola *ultimo;
+        int cont;
+
+    protected:
+
+    private:
+};
+
+#endif // COLA_H
---
+++ src/cola.cpp
@@ -0,0 +1,11 @@
+#include "cola.h"
+
+cola::cola()
+{
+    //ctor
+}
+
+cola::~cola()
+{
+    //dtor
+}
