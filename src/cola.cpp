f467d64eacae67f74a3d6e506f636d957e0ccd98
fedegy <fede88662@gmail.com>
2021-08-16 19:24:16 -0600

    Se agrego metodo para verificar cola vacia

--- src/cola.cpp
+++ src/cola.cpp
@@ -3,6 +3,13 @@
 cola::cola()
 {
     //ctor
+    this->primero=nullptr;
+    this->ultimo=nullptr;
+    this->cont=0;
+}
+
+int cola::cola_vacia(){
+    return this->primero==nullptr;
 }
 
 cola::~cola()
