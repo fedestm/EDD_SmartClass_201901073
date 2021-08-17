630cf933ba9cc4863dac5d583decf48c901d4b23
fedegy <fede88662@gmail.com>
2021-08-16 19:27:10 -0600

    Se agrego metodo encolar cola.cpp

--- src/cola.cpp
+++ src/cola.cpp
@@ -12,6 +12,18 @@ int cola::cola_vacia(){
     return this->primero==nullptr;
 }
 
+void cola::encolar(int id,string tipo,string descripcion){
+    nodo_cola *nuevo=new nodo_cola(nullptr,id,tipo,descripcion);
+    if(this->cola_vacia()){
+        this->primero=nuevo;
+        this->ultimo=nuevo;
+    }else{
+        this->ultimo->siguiente=nuevo;
+        this->ultimo=nuevo;
+    }
+    this->cont++;
+}
+
 cola::~cola()
 {
     //dtor
