373697d0e5f9988ffafc3240f1e3bfaabf47cd25
fedegy <fede88662@gmail.com>
2021-08-17 09:52:50 -0600

    Se agrego metodo graficar cola

--- src/cola.cpp
+++ src/cola.cpp
@@ -49,8 +49,33 @@ void cola::graficar_cola(){
         fs << "label = \"Cola de Errores\"; \n"<<endl;
         fs << "color= black \n"<<endl;
 
+        int cont_cola=0;
+        temp=this->primero;
+        while(temp!=nullptr){
+            //Mostrar el primer nodo
+            if(temp==this->primero){
+                fs<<"\t\n_"<<cont_cola<<"[label=\"Id: "<<temp->id<<"\\nTipo: "<<temp->tipo<<
+                "\\nDescripción: "<<temp->descripcion<<"\",shape=box,color=\"#F1350C\",fontcolor=white];\n"<<endl;
+            }else if(temp==this->ultimo){   //Mostrar el ultimo nodo
+                fs<<"\t\n_"<<cont_cola<<"[label=\"Id: "<<temp->id<<"\\nTipo: "<<temp->tipo<<
+                "\\nDescripción: "<<temp->descripcion<<"\",shape=box,color=\"#F1350C\",fontcolor=white];\n"<<endl;
+            }else{  //Mostrar los nodos que estan en medio
+                fs<<"\t\n_"<<cont_cola<<"[label=\"Id: "<<temp->id<<"\\nTipo: "<<temp->tipo<<
+                "\\nDescripción: "<<temp->descripcion<<"\",shape=box,color=\"#F1350C\",fontcolor=white];\n"<<endl;
+            }
+            temp=temp->siguiente;
+            cont_cola++;
+        }
+        
+        for(int i=0;i<cont_cola-1;i++){
+            fs<<"n_"<<i<<"->"<<"n_"<<i+1<<endl;
+        }
+
         fs<<"\t}\n"<<endl;
         fs.close();
+
+        system("dot Tpng cola.dot -o cola_errores.png");
+        system("cola_errores.png &");
     }
 }
 
@@ -58,3 +83,4 @@ cola::~cola()
 {
     //dtor
 }
+fs <<"\t\tn_"<<cont_g<< "[label = \"Dato: "<< temp->dato<<"\",shape=box,color=\"#63EA30\"];\n" <<endl;
