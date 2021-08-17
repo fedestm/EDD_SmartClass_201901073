c3f4db6eebfe185ccb4a337bfd9fa4908e03c12a
fedegy <fede88662@gmail.com>
2021-08-16 19:10:28 -0600

    Se agrego apuntador de cola y nodo

---
+++ include/nodo_cola.h
@@ -0,0 +1,23 @@
+#ifndef NODO_COLA_H
+#define NODO_COLA_H
+
+
+class nodo_cola
+{
+    public:
+        nodo_cola();
+        virtual ~nodo_cola();
+        //Apuntador siguiente
+        nodo_cola *siguiente;
+        int id;
+        string tipo,descripcion;
+
+        nodo_cola(nodo_cola *siguiente,int id,string tipo,string descripion);
+
+
+    protected:
+
+    private:
+};
+
+#endif // NODO_COLA_H
---
+++ src/nodo_cola.cpp
@@ -0,0 +1,11 @@
+#include "nodo_cola.h"
+
+nodo_cola::nodo_cola()
+{
+    //ctor
+}
+
+nodo_cola::~nodo_cola()
+{
+    //dtor
+}
