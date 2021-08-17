78c0b015999ac19d301e34e8ed2c4e2980dcdbeb
fedegy <fede88662@gmail.com>
2021-08-16 19:21:50 -0600

    Se agregaron metodos de cola.h

--- include/cola.h
+++ include/cola.h
@@ -11,6 +11,18 @@ class cola
         nodo_cola *ultimo;
         int cont;
 
+        //Función que verifica que la cola no este vacía
+        int cola_vacia();
+
+        //Metodo para encolar
+        void encolar(int id,string tipo,string descripcion);
+
+        //Metodo para desencolar
+        void desencolar();
+
+        //Metodo para graficar
+        void graficar_colar();
+
     protected:
 
     private:
