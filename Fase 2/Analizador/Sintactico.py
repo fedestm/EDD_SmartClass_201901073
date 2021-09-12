dc7330735e697bdc4874b351a54200eeb2b0b7cc
fedegy <fede88662@gmail.com>
2021-09-12 16:32:30 -0600

    Se agrego primera parte de gramatica ¿Elements? elementos ¿$Elements?

--- Fase 2/Analizador/Sintactico.py
+++ Fase 2/Analizador/Sintactico.py
@@ -1,2 +1,8 @@
 from lexer import tokens
 
+names = {}
+
+def p_statement_group(t):
+    'statement : LQUESTION TELEMENTS RQUESTION elementos LQUESTION DOLAR TELEMENTS RQUESTION'
+    #print("prueba")
+    t[0]=t[4]
