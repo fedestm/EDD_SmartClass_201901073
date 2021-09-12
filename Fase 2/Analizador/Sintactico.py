da7c51b222c843ea74181deb9d397d03087e9f31
fedegy <fede88662@gmail.com>
2021-09-12 16:39:53 -0600

    Se agrego produccion para tipo de elemento y contenido
    elementos : elementos elemento
                    |  elemento
    
    ¿element type="user"?
          elemento
    ¿$element?

--- Fase 2/Analizador/Sintactico.py
+++ Fase 2/Analizador/Sintactico.py
@@ -6,3 +6,9 @@ def p_statement_group(t):
     'statement : LQUESTION TELEMENTS RQUESTION elementos LQUESTION DOLAR TELEMENTS RQUESTION'
     #print("prueba")
     t[0]=t[4]
+
+def p_elementos_group(t):
+    """elementos : elementos elemento
+                |  elemento
+    """
+
