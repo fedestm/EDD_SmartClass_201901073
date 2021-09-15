3681c92c46d03c7a6ce3583ec507800ea5f60024
fedegy <fede88662@gmail.com>
2021-09-15 17:09:31 -0600

    Se agrego rotacion simple izquierda en avl

--- Fase 2/Estructuras/avl.py
+++ Fase 2/Estructuras/avl.py
@@ -13,6 +13,7 @@ class avl:
             return a2
     
     #Retorna fase de equilibrio de nodo
+    #O altura
     def f_equilibrio(self, nodo_avl):
         #Se verifica si existe el nodo
         if nodo_avl:
@@ -20,5 +21,17 @@ class avl:
         else:
             #De lo contrario se retorna -1
             return -1
-        
+
+    #Rotaciones
+    #Simple Izquierda
+    def R_izquierda(self, nodo_avl):
+        aux = nodo_avl.izquierda
+        nodo_avl.izquierda = aux.derecha
+        aux.derecha = nodo_avl
+        nodo_avl.altura = self.maximo(self.f_equilibrio(nodo_avl.derecha) , self.f_equilibrio(nodo_avl.izquierda)) + 1
+        aux.altura = self.maximo(nodo_avl.altura, self.f_equilibrio(nodo_avl.izquierda)) + 1
+        return aux
+
+
+
 
