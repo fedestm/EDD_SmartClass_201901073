c4d96b222f33fa39b136b7be775f306735ca1595
fedegy <fede88662@gmail.com>
2021-09-14 18:17:26 -0600

    Se agregaron datos que se guardaran en nodo avl

--- Fase 2/Estructuras/nodo_avl.py
+++ Fase 2/Estructuras/nodo_avl.py
@@ -1,5 +1,12 @@
 class nodo_avl:
-    def __init__(self):
+    def __init__(self,carnet,dpi,nombre,carrera,password,creditos,edad):
+        self.carnet=carnet
+        self.dpi=dpi
+        self.nombre=nombre
+        self.carrera=carrera
+        self.password=password
+        self.creditos=creditos
+        self.edad=edad
         self.izquierda=None
         self.derecha=None
         self.altura=0
