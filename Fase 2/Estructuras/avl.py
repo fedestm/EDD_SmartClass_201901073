14a75aa6e04893ec637e50ee4c396a27e170fcd6
fedegy <fede88662@gmail.com>
2021-09-15 16:12:55 -0600

    Se agrega funcion maximo para retornar fase de equilibrio mayor

--- Fase 2/Estructuras/avl.py
+++ Fase 2/Estructuras/avl.py
@@ -1,3 +1,11 @@
 class avl:
     def __init__(self):
         self.raiz=None
+
+    #Retornar fase de equilibrio mayor
+    #Se cuentan la cantidad de saltos a la derecha y el mas grande es el que retorna
+    def maximo(self,a1,a2):
+        if a1>a2:
+            return a1
+        else:
+            return a2
