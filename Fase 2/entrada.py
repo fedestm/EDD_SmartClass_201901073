67f8d5420dc67202094eac127d79ad80e687a362
fedegy <fede88662@gmail.com>
2021-09-16 17:32:01 -0600

    Se lee entrada json y se convierte en objetos

--- Fase 2/entrada.py
+++ Fase 2/entrada.py
@@ -1,10 +1,13 @@
 from Analizador.Sintactico import parser
 
 if __name__=="__main__":
+    """
     entrada = open("C:/Users/User/Documents/GitHub/EDD_SmartClass_201901073/Fase 2/Estudiantes.txt","r")
     #print(entrada.read())
     mensaje=entrada.read()
     entrada.close()
     
-
     analizar = parser.parse(mensaje)
+    """
+    file = open("C:/Users/User/Documents/GitHub/EDD_SmartClass_201901073/Fase 2/CursosEstudiante.json","r")
+    datos = json.load(file)
