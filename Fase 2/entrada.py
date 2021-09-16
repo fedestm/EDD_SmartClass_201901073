e1a07d8eed64d94a85fa855c3b4be668c7c2e686
fedegy <fede88662@gmail.com>
2021-09-16 17:39:36 -0600

    Se recorren json para organizar semestre segun carnet

--- Fase 2/entrada.py
+++ Fase 2/entrada.py
@@ -12,3 +12,16 @@ if __name__=="__main__":
     """
     file = open("C:/Users/User/Documents/GitHub/EDD_SmartClass_201901073/Fase 2/CursosEstudiante.json","r")
     datos = json.load(file)
+
+    for i in datos["Estudiantes"]:
+        print(i["Carnet"],end="\n")
+        for j in i["AÃ±os"]:
+            print(j["AÃ±o"])
+            for k in j["Semestres"]:
+                print("Semestre: ",end=k["Semestre"] + "\n")
+                for l in k["Cursos"]:
+                    print(l)
+        
+        print("\n")
+    
+    file.close()
