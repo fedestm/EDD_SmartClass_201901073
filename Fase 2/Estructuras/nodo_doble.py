b60d70d536b1a635353ae8e1b0a9f512c2ead2a6
fedegy <fede88662@gmail.com>
2021-09-16 18:26:36 -0600

    Se agrego nodo para lista doble Años

---
+++ Fase 2/Estructuras/nodo_doble.py
@@ -0,0 +1,7 @@
+class NodoDoble_Anios:
+    def __init__(self, anterior, siguiente, anio, semestre, meses):
+        self.anio = anio
+        self.semestre = semestre
+        self.meses = meses
+        self.anterior = anterior
+        self.siguiente = siguiente
