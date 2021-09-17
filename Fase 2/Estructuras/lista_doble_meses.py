ba03e6f2ee75daad03867476d290bc14fe560fac
fedegy <fede88662@gmail.com>
2021-09-16 18:59:45 -0600

    Se agrega metodo para verificar lista doble meses este vacia

--- Fase 2/Estructuras/lista_doble_meses.py
+++ Fase 2/Estructuras/lista_doble_meses.py
@@ -3,3 +3,6 @@ from nodo_doble import NodoDoble_Meses
 class ListaDoble_Meses:
     def __init__(self):
         self.primero = self.ultimo = None
+    
+    def lista_vacia(self):
+        return self.primero == None
