4fa41096499f42d0d0bf0a8bf597769b6a59666c
fedegy <fede88662@gmail.com>
2021-09-16 18:08:13 -0600

    Se guarda nodo simple en primero y ultimo si esta vacio

--- Fase 2/Estructuras/lista_simple.py
+++ Fase 2/Estructuras/lista_simple.py
@@ -1,6 +1,13 @@
+from nodo_simple import Nodo_Simple
+
 class ListaSimple:
     def __init__(self):
         self.primero = self.ultimo = None
     
     def lista_vacia(self):
         return self.primero == None
+    
+    def insertar(self, semestre, cursos):
+        if lista_vacia():
+            self.primero = self.ultimo = Nodo_Simple(None, semestre, cursos)
+    
