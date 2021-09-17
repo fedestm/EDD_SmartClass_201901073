e96b457d0c9e87f926edd30d85100f5e6bfd6aa2
fedegy <fede88662@gmail.com>
2021-09-16 18:38:41 -0600

    Se inserta nodo en primero y ultimo cuando esta vacia (lista doble años)

--- Fase 2/Estructuras/lista_doble_anios.py
+++ Fase 2/Estructuras/lista_doble_anios.py
@@ -1,6 +1,13 @@
+from nodo_doble import NodoDoble_Anios
+
 class ListaDoble_Anios:
     def __init__(self):
         self.primero = self.ultimo = None
     
     def lista_vacia(self):
         return self.primero == None
+    
+    def insertar(self, anio, semestre, meses):
+        nuevo = NodoDoble_Anios(None, None, anio, semestre, meses)
+        if lista_vacia():
+            self.primero = self.ultimo = nuevo
