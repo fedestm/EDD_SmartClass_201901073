7b42dff204c1757944edae7302c19d2c3b42b097
fedegy <fede88662@gmail.com>
2021-09-27 13:18:06 -0600

    Se enlazan claves con ListaNodos (Pagina.py)

--- Fase 2/Estructuras/ArbolB/Pagina.py
+++ Fase 2/Estructuras/ArbolB/Pagina.py
@@ -1,6 +1,9 @@
+from ListaNodos import ListaNodos
+
 class Pagina:
     def __init__(self):
         self.raiz = False
         self.tam = 0
         self.claves_max = 4
         self.claves_min = 2
+        self.claves = ListaNodos()
