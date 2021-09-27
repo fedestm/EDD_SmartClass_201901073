b20c3a6d63bd57367b3446297e979d18c741d358
fedegy <fede88662@gmail.com>
2021-09-27 12:21:47 -0600

    Se inserta nodo si primero esta vacio (ListaNodos.py)

--- Fase 2/Estructuras/ArbolB/ListaNodos.py
+++ Fase 2/Estructuras/ArbolB/ListaNodos.py
@@ -3,4 +3,11 @@ class ListaNodos:
         self.primero = None
         self.ultimo = None
         self.size = 0
-        
+    
+    def insertar(self, nuevo):
+        if self.primero == None:
+            self.primero = nuevo
+            self.ultimo = nuevo
+            self.size += 1
+            return True
+            
