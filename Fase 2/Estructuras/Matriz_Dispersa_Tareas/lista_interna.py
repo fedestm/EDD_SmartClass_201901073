a6f1af8dd7c5cf55a5d72d5ee3712b2269bf91cb
fedegy <fede88662@gmail.com>
2021-09-27 19:19:22 -0600

    Se inserta posicion x en nodo interno (lista_interna.py)

--- Fase 2/Estructuras/Matriz_Dispersa_Tareas/lista_interna.py
+++ Fase 2/Estructuras/Matriz_Dispersa_Tareas/lista_interna.py
@@ -3,3 +3,12 @@ from nodo_interno import NodoInterno
 class Lista_Interna:
     def __init__(self):
         self.primero = None
+    
+    def insertar_posx(self, x, y, cantidad):
+        nuevo = NodoInterno(x, y, cantidad)
+
+        if self.primero:
+            if nuevo.pos_y < self.primero.pos_y:
+                nuevo.siguiente = self.primero
+                self.primero.anterior = nuevo
+                self.primero = nuevo
