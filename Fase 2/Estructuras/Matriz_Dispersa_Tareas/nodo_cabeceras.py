63ad088b47d414b58f33d1afdf8279c95b1a1a5f
fedegy <fede88662@gmail.com>
2021-09-27 19:55:25 -0600

    Se enlaza clase lista interna a nodo (nodo_cabeceras.py)

--- Fase 2/Estructuras/Matriz_Dispersa_Tareas/nodo_cabeceras.py
+++ Fase 2/Estructuras/Matriz_Dispersa_Tareas/nodo_cabeceras.py
@@ -1,5 +1,8 @@
+from lista_interna import Lista_Interna
+
 class NodoCabeceras:
     def __init__(self, posicion):
         self.siguiente = None
         self.anterior = None
         self.posicion = posicion
+        self.lista_interna = Lista_Interna()
