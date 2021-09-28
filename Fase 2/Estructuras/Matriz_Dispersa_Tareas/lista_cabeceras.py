143d9202778618a64ce76ad0ace027016097c1bd
fedegy <fede88662@gmail.com>
2021-09-27 20:00:39 -0600

    Se verifica si nuevo nodo es menor al primero (lista_cabeceras.py)

--- Fase 2/Estructuras/Matriz_Dispersa_Tareas/lista_cabeceras.py
+++ Fase 2/Estructuras/Matriz_Dispersa_Tareas/lista_cabeceras.py
@@ -1,3 +1,10 @@
 class ListaCabeceras:
     def __init__(self):
         self.primero = None
+    
+    def insertar_cabecera(self, nuevo):
+        if self.primero:
+            if nuevo.posicion < self.primero.posicion:
+                nuevo.siguiente = self.primero
+                self.primero.anterior = nuevo
+                self.primero = nuevo
