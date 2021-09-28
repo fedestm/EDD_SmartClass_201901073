e1b6eb03b787b11e92b0b03c3c048efe7e05ef38
fedegy <fede88662@gmail.com>
2021-09-27 19:06:22 -0600

    Se corrigio nodo interno (nodo_intenno.py)

--- Fase 2/Estructuras/Matriz_Dispersa_Tareas/nodo_interno.py
+++ Fase 2/Estructuras/Matriz_Dispersa_Tareas/nodo_interno.py
@@ -1,7 +1,7 @@
 class NodoInterno:
-    def __init__(self, anterior, siguiente, pos_x, pos_y, cantidad):
-        self.anterior = anterior
-        self.siguiente = siguiente
+    def __init__(self, pos_x, pos_y, cantidad):
+        self.anterior = None
+        self.siguiente = None
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.arriba = None
