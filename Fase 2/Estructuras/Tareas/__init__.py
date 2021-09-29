14c1a73a047763a747d583021dbaa23da506c9e9
fedegy <fede88662@gmail.com>
2021-09-29 17:28:11 -0600

    Importacion de nodos y listas (__init__.py) Tareas

--- Fase 2/Estructuras/Tareas/__init__.py
+++ Fase 2/Estructuras/Tareas/__init__.py
@@ -2,3 +2,4 @@ from .nodo_interno import NodoInterno
 from .lista_interna import Lista_Interna
 from .lista_cabeceras import ListaCabeceras
 from .nodo_cabeceras import NodoCabeceras
+from .lista_doble_tareas import ListaDoble_Tareas
