59c12a3b3de77fd9a539bc71ccd513789b937173
fedegy <fede88662@gmail.com>
2021-09-28 20:16:49 -0600

    Se enlazo lista simple con Arbol B (nodo_simple.py)

--- Fase 2/Estructuras/nodo_simple.py
+++ Fase 2/Estructuras/nodo_simple.py
@@ -1,5 +1,8 @@
+from ArbolB.ArbolB import ArbolB
+
 class Nodo_Simple:
     def __init__(self,siguiente, semestre, cursos):
         self.siguiente = siguiente
         self.semestre = semestre
         self.cursos = cursos
+        self.arbol_cursos = ArbolB()
