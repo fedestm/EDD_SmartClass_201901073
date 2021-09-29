b195f7e7db8ff32ed56f787486caaf9af9d214a4
fedegy <fede88662@gmail.com>
2021-09-28 19:42:13 -0600

    Correcion de recorrido de temporal de cabecera en x (matriz.py)

--- Fase 2/Estructuras/Matriz_Dispersa_Tareas/matriz.py
+++ Fase 2/Estructuras/Matriz_Dispersa_Tareas/matriz.py
@@ -41,7 +41,7 @@ class Matriz:
             tempx = temp_cx.lista_interna.primero
             dot += "\n\tnode[label = \"X: %d\" fillcolor=\"#58D68D\" pos = \"%d,1!\" shape = box]x%d;" % (tempx.pos_x ,temp.pos_x, cont)
             cont += 1
-            temp = temp.siguiente
+            temp_cx = temp_cx.siguiente
         
         #Cabeceras en Y
         temp_cy = self.cabeceras_y.primero
