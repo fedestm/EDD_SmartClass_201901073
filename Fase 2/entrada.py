1bd9bd13ed62f4f291f47f77e57565b89279a5fa
fedegy <fede88662@gmail.com>
2021-09-30 17:02:52 -0600

    Se analiza archivo de entrada Estudiantes.txt

--- Fase 2/entrada.py
+++ Fase 2/entrada.py
@@ -4,3 +4,9 @@ import json
 
 a = avl()
 
+class CRUD:
+    def cargar_estudiantes(self, ruta):
+        entrada = open(ruta)
+        data = entrada.read()
+        entrada.close()
+        analizar = parser.parse(data)
