338ec02e08867834014dc15e09285f4cc6f3182d
fedegy <fede88662@gmail.com>
2021-09-29 19:38:06 -0600

    Se insertan tareas a lista doble (avl.py)

--- Fase 2/Estructuras/avl.py
+++ Fase 2/Estructuras/avl.py
@@ -180,4 +180,9 @@ class avl:
         if temp != None:
             temp.lista_anios.insertar_mes_matriz(anio, mes, dia, hora)
     
+    def insertar_tareas_matriz(self, carnet, anio, mes, dia_y, hora_y, carnet_t, nombre, desc, materia, fecha, hora, estado):
+        temp = self.buscar(carnet)
+        if temp != None:
+            temp.lista_anios.insertar_tareas_matriz(anio, mes, dia_y, hora_y, carnet_t, nombre, desc, materia, fecha, hora, estado)
+    
 
