941c220440a3c4491bce916c4728b353630c92e8
fedegy <fede88662@gmail.com>
2021-10-01 14:58:21 -0600

    Se enlaza arbol B con nodo simple de cursos de pensum (lista_pensum.py)

--- Fase 2/Estructuras/__init__.py
+++ Fase 2/Estructuras/__init__.py
@@ -6,3 +6,4 @@ from .nodo_avl import nodo_avl
 from .nodo_doble_meses import NodoDoble_Meses
 from .nodo_doble import NodoDoble_Anios
 from .nodo_simple import Nodo_Simple
+from .lista_pensum import NodoSimple_ArbolB
--- Fase 2/Estructuras/lista_pensum.py
+++ Fase 2/Estructuras/lista_pensum.py
@@ -1,5 +1,7 @@
+from .ArbolB import ArbolB
 
 class NodoSimple_ArbolB:
     def __init__(self, siguiente, cursos):
         self.siguiente = siguiente
         self.cursos = cursos
+        self.arbol_pensum = ArbolB()
