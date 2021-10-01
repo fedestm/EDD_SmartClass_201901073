ab8dc0cef47f0991d2a3c32fc23a8317544a6985
fedegy <fede88662@gmail.com>
2021-10-01 15:13:46 -0600

    Se agrego metodo para insertar cursos (lista_pensum.py)

--- Fase 2/Estructuras/lista_pensum.py
+++ Fase 2/Estructuras/lista_pensum.py
@@ -10,4 +10,15 @@ class ListaSimple_Pensum:
     def __init__(self):
         self.primero = self.ultimo = None
     
+    def lista_vacia(self):
+        return self.primero == None
+    
+    def insertar(self, cursos):
+        nuevo = NodoSimple_ArbolB(None, cursos)
+        if self.lista_vacia():
+            print("Lista Vacia")
+        else:
+            self.ultimo.siguiente = nuevo
+            self.ultimo = nuevo
+    
     
