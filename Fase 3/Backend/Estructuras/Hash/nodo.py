61d49ff36855082260ff8eea2f2f368af656359f
fedegy <fede88662@gmail.com>
2021-10-19 18:09:48 -0600

    Se agrego nodo hash (nodo.py)

---
+++ Fase 3/Backend/Estructuras/Hash/nodo.py
@@ -0,0 +1,9 @@
+class Nodo:
+    def __init__(self, carnet, dpi, nombre, carrera, correo, password, edad):
+        self.carnet = carnet
+        self.dpi = dpi
+        self.nombre = nombre
+        self.carrera = carrera
+        self.correo = correo
+        self.password = password
+        self.edad = edad
