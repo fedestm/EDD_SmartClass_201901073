c80ad51c5a87f33d8b1846bd97ceab614f1bcd13
fedegy <fede88662@gmail.com>
2021-10-20 20:08:15 -0600

    Archivos javascript necesarios

---
+++ Fase 3/Frontend/js/init.js
@@ -0,0 +1,124 @@
+
+
+(function($) {
+
+	skel.init({
+		reset: 'full',
+		breakpoints: {
+			
+			// Global.
+				global: {
+					range: '*',
+					href: 'style/style_juegos.css',
+					containers: 1400,
+					grid: {
+						gutters: {
+							vertical: '4em',
+							horizontal: 0
+						}
+					}
+				},
+
+			// XLarge.
+				xlarge: {
+					range: '-1680',
+					href: 'style/style-xlarge.css',
+					containers: 1200
+				},
+
+			// Large.
+				large: {
+					range: '-1280',
+					href: 'style/style-large.css',
+					containers: 960,
+					grid: {
+						gutters: {
+							vertical: '2.5em'
+						}
+					},
+					viewport: {
+						scalable: false
+					}
+				},large: {
+					range: '-1280',
+					href: 'style/style-large.css',
+					containers: 960,
+					grid: {
+						gutters: {
+							vertical: '2.5em'
+						}
+					},
+					viewport: {
+						scalable: false
+					}
+				},
+
+			// Medium.
+				medium: {
+					range: '-980',
+					href: 'style/style-medium.css',
+					containers: '90%',
+					grid: {
+						collapse: 1
+					}
+				},
+
+			// Small.
+				small: {
+					range: '-736',
+					href: 'style/style-small.css',
+					containers: '90%',
+					grid: {
+						gutters: {
+							vertical: '1.25em'
+						}
+					}
+				},
+
+			// XSmall.
+				xsmall: {
+					range: '-480',
+					href: 'style/style-xsmall.css',
+					grid: {
+						collapse: 2
+					}
+				}
+
+		},
+		plugins: {
+			layers: {
+
+					config: {
+						transform: true
+					},
+					navPanel: {
+						animation: 'pushX',
+						breakpoints: 'medium',
+						clickToHide: true,
+						height: '100%',
+						hidden: true,
+						html: '<div data-action="moveElement" data-args="nav"></div>',
+						orientation: 'vertical',
+						position: 'top-left',
+						side: 'left',
+						width: 250
+					},
+
+					navButton: {
+						breakpoints: 'medium',
+						height: '4em',
+						html: '<span class="toggle" data-action="toggleLayer" data-args="navPanel"></span>',
+						position: 'top-left',
+						side: 'top',
+						width: '6em'
+					}
+
+			}
+		}
+	});
+
+	$(function() {
+	
+	});
+
+})(jQuery);
