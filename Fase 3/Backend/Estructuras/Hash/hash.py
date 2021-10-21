85a7c17fcd320e07ddd25a806feb3f3ff0530bde
fedegy <fede88662@gmail.com>
2021-10-20 18:19:06 -0600

    Se agrego solucion de colisiones (hash.py)

--- Fase 3/Backend/Estructuras/Hash/hash.py
+++ Fase 3/Backend/Estructuras/Hash/hash.py
@@ -10,3 +10,20 @@ class Hash:
         resultado = 0
         resultado = carnet % self.size
         return resultado
+    
+    def solucionar_colisiones(self, indice):
+        nuevo_indice = 0
+        i = 0
+        disponible = False
+
+        while disponible == False:
+            nuevo_indice = indice + int(i**2)
+
+            if nuevo_indice >= self.size:
+                nuevo_indice = nuevo_indice - self.size
+            if self.claves[nuevo_indice] == None:
+                disponible = True
+            i += 1
+        return nuevo_indice
+    
+    
