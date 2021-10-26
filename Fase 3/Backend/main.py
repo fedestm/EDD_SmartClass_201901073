94f13c8bad7bc76bc7030872664a4d4354138fa5
fedegy <fede88662@gmail.com>
2021-10-25 18:18:32 -0600

    Se agrego peticion para insertar estudiante (main.py)

--- Fase 3/Backend/main.py
+++ Fase 3/Backend/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from werkzeug.wrappers import response
 from Estructuras import CRUD
 
 crud = CRUD
@@ -8,5 +9,18 @@ app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app)
 
+@app.route("/insertar_estudiante", methods = ['POST'])
+def insertar_estudiante():
+    if request.method == 'POST':
+        carnet = request.json['carnet']
+        dpi = request.json['dpi']
+        nombre = request.json['nombre']
+        carrera = request.json['carrera']
+        correo = request.json['correo']
+        password = request.json['password']
+        edad = request.json['edad']
+        crud.registrar_estudiante(carnet, dpi, nombre, carrera, correo, password, edad)
+        response = jsonify({'response': 'Se registro estudiante'})
+        return response
 
 
