18747e1a4251dd50e159364e8c75ed57bb6c027f
fedegy <fede88662@gmail.com>
2021-10-26 19:41:05 -0600

    Se agrego funcion para insertar apunte desde Frontend (smartclass.js)

--- Fase 3/Frontend/js/smartclass.js
+++ Fase 3/Frontend/js/smartclass.js
@@ -2,3 +2,28 @@ function storage_usuario(){
     var user = localStorage.getItem("Usuario")
     document.getElementById("txtcarnet").value = user
 }
+
+function insertar_apunte(){
+    var carnet = document.getElementById("txtcarnet").value
+    var titulo = document.getElementById("txttitulo").value
+    var contenido = document.getElementById("txtcontenido").value
+
+    var datos = JSON.stringify({
+        "carnet": carnet,
+        "titulo": titulo,
+        "contenido": contenido
+    });
+
+    fetch('http://localhost:3000/insertar_apunte', {
+        method: 'post',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: datos
+    })
+    .then(response => response.json())
+    .then(datos => {
+        console.log(datos)
+    alert(datos.response)
+    })
+}
