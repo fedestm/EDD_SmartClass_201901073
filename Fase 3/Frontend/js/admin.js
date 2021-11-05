98f4d912d4a58ef35008c66f735ec88b5b71b48d
fedegy <fede88662@gmail.com>
2021-11-04 19:26:07 -0600

    Se agrego funcion para graficar grafo en javascript (admin.js)

--- Fase 3/Frontend/js/admin.js
+++ Fase 3/Frontend/js/admin.js
@@ -13,3 +13,19 @@ function reporte_hash(){
     console.log(img)
     })
 }
+
+function reporte_grafo(){
+    fetch('http://localhost:3000/graficar_grafo', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        img = data.img
+        document.getElementById("img").innerHTML = "<img src = \"data:image/png;base64," + img + "\">"
+        console.log(img)
+    })
+}
