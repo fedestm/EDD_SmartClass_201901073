f93bfde84c623adda313e9a8aadee46bc349561e
fedegy <fede88662@gmail.com>
2021-11-06 10:02:51 -0600

    Se agrega lista simple pensum para Arbol B de Fase 2 (lista_simple_pensum.py)

---
+++ Fase 3/Backend/Estructuras/Hash/lista_simple_pensum.py
@@ -0,0 +1,44 @@
+from .ArbolB import ArbolB
+
+class NodoSimple_ArbolB:
+    def __init__(self, siguiente, cursos):
+        self.siguiente = siguiente
+        self.cursos = cursos
+        self.arbol_pensum = ArbolB()
+
+class ListaSimple_Pensum:
+    def __init__(self):
+        self.primero = self.ultimo = None
+    
+    def lista_vacia(self):
+        return self.primero == None
+    
+    def insertar(self, cursos):
+        nuevo = NodoSimple_ArbolB(None, cursos)
+        if self.lista_vacia():
+            self.primero = self.ultimo = nuevo
+        else:
+            self.ultimo.siguiente = nuevo
+            self.ultimo = nuevo
+    
+    def buscar(self, cursos):
+        if self.lista_vacia():
+            print("Lista Vacia")
+        else:
+            temp = self.primero
+            while temp != None:
+                if temp.cursos == cursos:
+                    return temp
+                else:
+                    temp = temp.siguiente
+            return None
+    
+    def insertar_cursos(self, cursos, codigo, nombre, creditos, prerequisitos, obligatorio):
+        temp = self.buscar(cursos)
+        if temp != None:
+            temp.arbol_pensum.insertar_nodo(codigo, nombre, creditos, prerequisitos, obligatorio)
+
+    def graficar_arbolPensum(self, tipo):
+        temp = self.buscar(tipo)
+        if temp != None:
+            temp.arbol_pensum.graficar()
