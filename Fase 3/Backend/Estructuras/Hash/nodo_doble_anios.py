e1420c6cd2a029fbd4fe9da3183785b356cc9a41
fedegy <fede88662@gmail.com>
2021-11-05 18:24:15 -0600

    Se enlaza lista simple con lista doble (nodo_doble_anios.py)

--- Fase 3/Backend/Estructuras/Hash/nodo_doble_anios.py
+++ Fase 3/Backend/Estructuras/Hash/nodo_doble_anios.py
@@ -1,5 +1,8 @@
+from .lista_simple_semestre import ListaSimple
+
 class NodoDoble_Anios:
     def __init__(self, anterior, siguiente, anio):
         self.anio = anio
         self.anterior = anterior
         self.siguiente = siguiente
+        self.lista_anios = ListaSimple()
