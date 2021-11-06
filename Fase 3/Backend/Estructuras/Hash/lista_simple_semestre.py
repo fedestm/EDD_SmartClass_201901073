321a9be1ce737c360260069e487a64e2e923cf97
fedegy <fede88662@gmail.com>
2021-11-05 18:22:15 -0600

    Se agrega lista simple semestre de Fase 2 (lista_simple_semestre.py)

---
+++ Fase 3/Backend/Estructuras/Hash/lista_simple_semestre.py
@@ -0,0 +1,38 @@
+from .nodo_simple_semestre import Nodo_Simple
+
+class ListaSimple:
+    def __init__(self):
+        self.primero = self.ultimo = None
+    
+    def lista_vacia(self):
+        return self.primero == None
+    
+    def insertar(self, semestre):
+        nuevo = Nodo_Simple(None, semestre)
+        if self.lista_vacia():
+            self.primero = self.ultimo = nuevo
+        else:
+            self.ultimo.siguiente = nuevo
+            self.ultimo = nuevo
+    
+    def buscar(self, semestre):
+        if self.lista_vacia():
+            print("Lista Vacia")
+        else:
+            temp = self.primero
+            while temp != None:
+                if temp.semestre == semestre:
+                    return temp
+                else:
+                    temp = temp.siguiente
+            return None
+    
+    def insertar_cursos(self, semestre, codigo, nombre, creditos, prerequisitos, obligatorio):
+        temp = self.buscar(semestre)
+        if temp != None:
+            temp.arbol_cursos.insertar_nodo(codigo, nombre, creditos, prerequisitos, obligatorio)
+    
+    def graficar_arbolB(self, semestre):
+        temp = self.buscar(semestre)
+        if temp != None:
+            temp.arbol_cursos.graficar()
